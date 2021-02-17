a64a29167339de468033b27e62eaa82505766f60
htr-tech <tahmidrayat@gmailcom>
2021-02-17 21:22:34 +0600

    Uninstaller Added

---
+++ remove.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+R="$(printf '\033[1;31m')"
+G="$(printf '\033[1;32m')"
+Y="$(printf '\033[1;33m')"
+B="$(printf '\033[1;34m')"
+C="$(printf '\033[1;36m')"
+W="$(printf '\033[1;37m')"
+
+banner() {
+    clear
+    printf "\033[33m    _  _ ___  _  _ _  _ ___ _  _    _  _ ____ ___  \033[0m\n"
+    printf "\033[36m    |  | |__] |  | |\ |  |  |  |    |\/| |  | |  \ \033[0m\n"
+    printf "\033[32m    |__| |__] |__| | \|  |  |__|    |  | |__| |__/ \033[0m\n"
+    printf "\033[0m\n"
+    printf "     \033[32mA modded gui version of ubuntu for Termux\033[0m\n"
+    printf "\033[0m\n"
+
+}
+
+package() {
+    echo -e "${R} [${W}-${R}]${C} Purging packages..."${W}
+    proot-distro remove ubuntu-18.04
+    rm -rf $PREFIX/bin/ubuntu
+    sed 's+pulseaudio+#pulseaudio+g' .bashrc > .bashrc1
+    mv .bashrc1 .bashrc
+    sleep 1
+    sed 's+pacmd+#pacmd+g' .bashrc > .bashrc1
+    mv .bashrc1 .bashrc
+    sleep 1
+    echo -e "${R} [${W}-${R}]${C} Purging Completed !"${W}
+
+
+
+
+}
+
+banner
+package
+
