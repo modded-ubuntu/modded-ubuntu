e964b96ad0b7983850efa20eb0c4c8f371b177ac
BDhaCkers009 <ahmedshifat1234@gmail.com>
2022-03-19 18:21:42 +0600

    fix subl

--- distro/gui.sh
+++ distro/gui.sh
@@ -143,6 +143,7 @@ sublime_installer() {
 	echo 
 	echo "installing Sublime Text Editor.."
 	echo
+	sudo apt install  software-properties-common gnupg --no-install-recommends -y
 	wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 	sudo apt-get install apt-transport-https
 	echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
