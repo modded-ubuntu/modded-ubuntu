59941400e5d3ef278fc32ab35974898b4c644251
BDhaCkers009 <ahmedshifat1234@gmail.com>
2022-04-16 06:04:54 +0600

    upd usermsh

--- distro/user.sh
+++ distro/user.sh
@@ -29,8 +29,11 @@ login() {
     banner
     read -p $' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Input Username [Lowercase] : \e[0m\e[1;96m\en' user
     echo -e "${W}"
+    read -p $' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Input Password : \e[0m\e[1;96m\en' pass
+    echo -e "${W}"
+    useradd -m ${user}
+    echo "${user}:${pass}" | chpasswd
     echo "$user ALL=(ALL:ALL) ALL" >> /etc/sudoers
-    adduser $user
     echo "proot-distro login --user $user ubuntu --bind /dev/null:/proc/sys/kernel/cap_last_last --shared-tmp --fix-low-ports" > /data/data/com.termux/files/usr/bin/ubuntu
     #chmod +x /data/data/com.termux/files/usr/bin/ubuntu 
     wget https://raw.githubusercontent.com/modded-ubuntu/modded-ubuntu/master/distro/gui.sh
