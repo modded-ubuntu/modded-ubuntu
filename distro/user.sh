ec9df928d30169ca585f4a9b1d1a64cebe8e63c6
BDhaCkers009 <ahmedshifat1234@gmail.com>
2022-12-31 19:02:15 +0600

    gui.sh path

--- distro/user.sh
+++ distro/user.sh
@@ -36,7 +36,7 @@ login() {
     echo "$user ALL=(ALL:ALL) ALL" >> /etc/sudoers
     echo "proot-distro login --user $user ubuntu --bind /dev/null:/proc/sys/kernel/cap_last_last --shared-tmp" > /data/data/com.termux/files/usr/bin/ubuntu
     #chmod +x /data/data/com.termux/files/usr/bin/ubuntu 
-    if [[ -f '/data/data/com.termux/files/home/modded-ubuntu/distro/gui.sh' ]];then
+    if [[ -e '/data/data/com.termux/files/home/modded-ubuntu/distro/gui.sh' ]];then
         cp /data/data/com.termux/files/home/modded-ubuntu/distro/gui.sh /home/$user/gui.sh
         chmod +x /home/$user/gui.sh
     else
