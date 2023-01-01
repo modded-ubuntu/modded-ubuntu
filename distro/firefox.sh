18d88fe3a76f96bb94bd71862e2c15c3ef140478
Mustakim Ahmed Sifat <67186139+BDhackers009@users.noreply.github.com>
2023-01-01 14:21:42 +0600

    Create firefox.sh

---
+++ distro/firefox.sh
@@ -0,0 +1,14 @@
+sudo snap remove firefox
+
+sudo add-apt-repository ppa:mozillateam/ppa
+
+echo '
+Package: *
+Pin: release o=LP-PPA-mozillateam
+Pin-Priority: 1001
+' | sudo tee /etc/apt/preferences.d/mozilla-firefox
+
+echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' | sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
+
+sudo apt install firefox -y
+
