8cff92924457b0d60b6fcf8b7b088f1c64fecf43
Mustakim Ahmed Sifat <67186139+BDhackers009@users.noreply.github.com>
2023-04-23 13:13:34 +0800

    fix exit error

--- distro/user.sh
+++ distro/user.sh
@@ -36,7 +36,7 @@ login() {
     usermod -aG sudo ${user}
     echo "${user}:${pass}" | chpasswd
     echo "$user ALL=(ALL:ALL) NOPASSWD:ALL" >> /etc/sudoers
-    echo "proot-distro login --user $user ubuntu --bind /dev/null:/proc/sys/kernel/cap_last_last --shared-tmp --fix-low-ports" > /data/data/com.termux/files/usr/bin/ubuntu
+    printf "#!/data/data/com.termux/files/usr/bin\nproot-distro login --user $user %s ubuntu --bind /dev/null:/proc/sys/kernel/cap_last_last --shared-tmp --fix-low-ports\n" > /data/data/com.termux/files/usr/bin/ubuntu
     #chmod +x /data/data/com.termux/files/usr/bin/ubuntu 
     
     if [[ -e '/data/data/com.termux/files/home/modded-ubuntu/distro/gui.sh' ]];then
