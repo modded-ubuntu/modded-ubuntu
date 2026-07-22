31fcce4dfbef3a176684bdae27d01b047d800fc5
​★- Superchavo -​★ <superchavoofficialscratchyt@gmail.com>
2026-07-22 16:21:09 -0600

    Enhance chromium.sh with logging and wrapper script
    Added logging for Xtradeb installation and created a wrapper script for Chromium.

--- distro/chromium.sh
+++ distro/chromium.sh
@@ -1,7 +1,13 @@
 echo "Installing Xtradeb first"
 wget -qO- https://raw.githubusercontent.com/modded-ubuntu/modded-ubuntu/refs/heads/test-ubuntu-26.04/distro/setup_xtradeb.sh | bash
+echo "Setup details saved to /tmp/xtradeb_install.log"
 echo "now installing chromium"
 apt update
 apt install chromium
+cat << 'EOF' > /usr/local/bin/chromium
+#!/bin/sh
+exec /usr/bin/chromium --no-sandbox "$@"
+EOF
+chmod +x /usr/local/bin/chromium
 echo "Ready!! now lets return to the main installation"
 
